5315aafd1cc69d1486988aacf1e5d2754a4758ab
jeishiva <s.sivaguru11@gmail.com>
2025-06-09 17:05:53 +0530

    added top bar

--- app/src/main/java/com/experiment/facedetector/ui/screen/FullImageScreen.kt
+++ app/src/main/java/com/experiment/facedetector/ui/screen/FullImageScreen.kt
@@ -4,9 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -16,13 +22,21 @@ import androidx.compose.ui.unit.dp
 import com.experiment.facedetector.viewmodel.FullImageViewModel
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FullImageScreen(mediaId: Long) {
     val viewModel: FullImageViewModel = koinViewModel()
     val mediaState = viewModel.media.collectAsState()
 
     Scaffold(
-        containerColor = Color.Transparent, modifier = Modifier.fillMaxSize()
+        topBar = {
+            TopAppBar(title = { Text("Full Image") }, navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
+            })
+        },
+        containerColor = Color.Transparent, modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
         Box(
             modifier = Modifier
